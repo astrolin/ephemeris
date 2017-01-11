eb63c14249ba6cf9d1c6b2038520b616954cad84
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-11 21:31:07 +0200

    bin/precompile script with a shell plugin :prep-tasks which doesn't seem to run it

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :min-lein-version  "2.0.0"
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java/swisseph"]
+  :prep-tasks [["shell" "bin/precompile"] "javac" "compile"]
   :plugins [[lein-shell "0.5.0"]])
