5b59240eb7c930168cc784100887f257a5b2eb81
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-11 17:24:05 +0200

    defproject and add swisseph-2.01.00-java-src-01 - Java port of Swiss Ephemeris by Thomas Mack

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject swejar "0.1.0-SNAPSHOT"
+  :description "Swiss Ephemeris for Clojure"
+  :min-lein-version  "2.0.0"
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"])
