bddaee255ead13d530235a676ca198f3709ac816
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-11 19:00:38 +0200

    project.clj and .gitignore tweaks

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject swejar "0.1.0-SNAPSHOT"
   :description "Swiss Ephemeris for Clojure"
   :min-lein-version  "2.0.0"
-  :source-paths      ["src/clojure"]
-  :java-source-paths ["src/java"])
+  :source-paths      ["src/clj"]
+  :java-source-paths ["src/java/swisseph"]
+  :plugins [[lein-shell "0.5.0"]])
