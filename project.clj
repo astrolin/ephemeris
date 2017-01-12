fbf9810f63506b6af7eda59ba3118e8cd7fcc998
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-12 10:12:13 +0200

    proto-repl for atom development

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
   :java-source-paths ["src/java/swisseph"]
   :prep-tasks [["shell" "bin/precompile"] "javac" "compile"]
   :plugins [[lein-shell "0.5.0"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]]
   :repl-options {:init-ns ephemeris.core})
