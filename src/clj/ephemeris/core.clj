2eee0721e809d3b4c8be5656576a4e943fde67c0
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-12 12:23:53 +0200

    swe_calc_ut starting point / works

--- src/clj/ephemeris/core.clj
+++ src/clj/ephemeris/core.clj
@@ -1,2 +1,22 @@
 (ns ephemeris.core
   (:import (swisseph SwissEph SweDate SweConst)))
+
+(defn calc-now [what]
+  (let [sw (SwissEph.)
+        sd (SweDate.)
+        jd (.getJulDay sd)
+        res (double-array 6)
+        err (StringBuffer.)]
+    (do
+      (.swe_calc_ut sw jd
+                    what
+                    (. SweConst SEFLG_SPEED)
+                    res
+                    err)
+      {what
+        {:lon (aget res 0)
+         :lat (aget res 1)
+         :lsd (aget res 3)}})))
+
+(comment
+  (calc-now (. SweConst SE_MARS)))
