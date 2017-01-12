e8ba90d3f7c5abe3543e7a1e2cc9cb2dd6caa6b6
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-12 17:05:25 +0200

    in case I write some java

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java/swisseph"]
   :prep-tasks [["shell" "bin/precompile"] "javac" "compile"]
-  :plugins [[lein-shell "0.5.0"]]
+  :plugins [[lein-shell "0.5.0"]
+            [lein-virgil "0.1.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl "0.3.1"]]
   :repl-options {:init-ns ephemeris.core})
