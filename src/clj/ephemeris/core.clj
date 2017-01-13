556fd59871757f38150ebd7a7a8718abd4c963e8
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-13 17:20:32 +0200

    prepare to get more than just body calculations; merge with defaults for simpler calls / validation

--- src/clj/ephemeris/core.clj
+++ src/clj/ephemeris/core.clj
@@ -2,13 +2,19 @@
   (:require [ephemeris.points :refer (lookup known?)])
   (:import (swisseph SwissEph SweDate SweConst)))
 
+(def defaults {:utc nil
+               :geo {:lat nil :lon nil}
+               :bodies []
+               :angles []
+               :houses false})
+
 (defn calc-now [stuff]
-  (let [s (flatten [stuff])
+  (let [want (merge defaults stuff)
         sw (SwissEph.)
         sd (SweDate.)
         jd (.getJulDay sd)
         flag (. SweConst SEFLG_SPEED)]
-    (for [i s]
+    (for [i (flatten [(:bodies want)])]
       (let [what (if (known? i) (lookup i) i)
             res (double-array 6)
             err (StringBuffer.)
