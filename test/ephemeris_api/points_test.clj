d5037676716eedf83cee1ff71fd4c9422c69dad5
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-16 19:51:33 +0200

    test ephemeris-api.points

---
+++ test/ephemeris_api/points_test.clj
@@ -0,0 +1,12 @@
+(ns ephemeris-api.points-test
+  (:require [clojure.test :refer :all]
+            [ephemeris-api.points :as ps]))
+
+(deftest points-test
+  (is (= 0 (ps/lookup :Sun)))
+  (is (= :Sun (ps/lookup 0)))
+  (is (nil? (ps/lookup :Unknown)))
+  (is (true? (ps/known? :Sun)))
+  (is (false? (ps/known? :Unknown)))
+  (is (= "Helios" (ps/nameit :Sun)))
+  (is (= "Sun" (ps/nameit :Sun :en))))
