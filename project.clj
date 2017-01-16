3a5cf3e7629c7252104d7d457ebaf4bd35608467
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-16 19:11:15 +0200

    fix path as dashes become underscores; add dev & repl profiles

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :min-lein-version  "2.0.0"
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java/swisseph"]
-  :prep-tasks [["shell" "bin/precompile"] "javac" "compile"]
-  :plugins [[lein-shell "0.5.0"]
-            [lein-virgil "0.1.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl "0.3.1"]
                  [clj-time "0.13.0"]]
-  :repl-options {:init-ns ephemeris-api.core})
+  :profiles
+    {:dev
+      {:dependencies [[proto-repl "0.3.1"]]
+       :plugins [[lein-shell "0.5.0"]]
+       :prep-tasks [["shell" "bin/precompile"] "javac" "compile"]}
+     :repl
+      {:repl-options {:init-ns ephemeris-api.core}}})
